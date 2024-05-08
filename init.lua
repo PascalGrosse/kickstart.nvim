42de0b71e4fe3b6ca5c333531a135a0a21f2c63c
Pascal Grossé <pascal.grosse@gmail.com>
2024-05-08 11:54:09 +0200

    Added new plugins

--- init.lua
+++ init.lua
@@ -113,7 +113,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv 'HOME' .. '.config/nvim/undodir'
+vim.opt.undodir = os.getenv 'HOME' .. '/.config/nvim/undodir'
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -609,6 +609,7 @@ require('lazy').setup({
             },
           },
         },
+        rescriptls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -796,21 +797,12 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-frappe'
     end,
   },
 
@@ -886,6 +878,14 @@ require('lazy').setup({
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
+  },
+  {
+    'rescript-lang/vim-rescript',
+    ft = 'rescript',
+  },
+  {
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
   },
   {
     'lervag/vimtex',
